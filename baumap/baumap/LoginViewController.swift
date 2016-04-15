f09989f95648e097a697e6c5a7898e8d4b8f3e54
Antonio Sejas <antonio@sejas.es>
2016-04-15 19:59:03 +0100

    passing the session to tab controller

--- baumap/baumap/LoginViewController.swift
+++ baumap/baumap/LoginViewController.swift
@@ -18,30 +18,37 @@ class LoginViewController: UIViewController {
         //Clean data
         guard let email = tfEmail.text,
             let password = tfPassword.text else {
-                //TODO: Inform the error in credentials "format"
+                showError("Error", message: "Please fill your user password")
                 return
         }
         
         print("loging with user\(email) password:\(password)")
         UdacityClient.sharedInstance().authenticate(email, password: password) { (session, error) in
             guard nil == error else {
-                //TODO: Inform the error authenticating
+                self.showError("Error", message: "Error: \(error)")
                 print("error",error)
                 return
             }
             print(session)
+            self.performSegueWithIdentifier("toTabView", sender: session.session_id)
         }
-        
-        //TODO: Uncomment after login
-        //performSegueWithIdentifier("toTabView", sender: nil)
     }
     @IBAction func actionFacebookLogin(sender: AnyObject) {
+        //TODO: Facebook login
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func showError(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message:
+            message, preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default,handler: nil))
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
     }
 
 }
