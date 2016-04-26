1db2de06f628d21c20692564d5dac5edd184b7ba
Antonio Sejas <antonio@sejas.es>
2016-04-26 23:49:34 +0100

    StudentLocation struct

---
+++ baumap/baumap/StudentLocation.swift
@@ -0,0 +1,17 @@
+//
+//  StudentLocation.swift
+//  baumap
+//
+//  Created by Antonio Sejas on 26/4/16.
+//  Copyright © 2016 Antonio Sejas. All rights reserved.
+//
+
+import MapKit
+
+struct StudentLocation {
+    var coordinate: CLLocationCoordinate2D
+    var first: String
+    var last: String
+    var mediaURL:  String
+
+}
---
+++ baumap/baumap/StudentLocations.swift
@@ -0,0 +1,13 @@
+//
+//  StudentLocations.swift
+//  baumap
+//
+//  Created by Antonio Sejas on 26/4/16.
+//  Copyright © 2016 Antonio Sejas. All rights reserved.
+//
+
+import UIKit
+
+class StudentLocations: NSObject {
+
+}
